b96ff1e214397dcd9f7f9332a90bc007d94466b5
Alleegg <tanuskin798@gmail.com>
2026-03-25 14:41:06 +0000

    №3.2

--- 3/step_5/task_5/metrics.py
+++ 3/step_5/task_5/metrics.py
@@ -1,37 +1,64 @@
+import numpy as np
+
 def binary_classification_metrics(prediction, ground_truth):
-    '''
-    Computes metrics for binary classification
 
-    Arguments:
-    prediction, np array of bool (num_samples) - model predictions
-    ground_truth, np array of bool (num_samples) - true labels
+    if len(prediction) == len(ground_truth):
 
-    Returns:
-    precision, recall, f1, accuracy - classification metrics
-    '''
-    precision = 0
-    recall = 0
-    accuracy = 0
-    f1 = 0
+        #main == np.zeros((2, 2), np.int_)
+        TP, TN, FN, FP = 0,0,0,0
+        for pred in range(prediction):
+            for truth in range(ground_truth):
+                if pred == 0 and truth == 0:
+                    TN += 1
+                elif pred == 1 and truth == 1:
+                    TP += 1
+                elif pred == 0 and truth == 1:
+                    FN += 1
+                elif pred == 1 and truth == 0:
+                    FP += 1
+                
+        # main = np.array([TP, FN],
+        #                 [FP, TN])
+        '''
+        Аргументы:
+        prediction, np-массив значений bool (num_samples) - предсказания модели
+        ground_truth, np-массив значений bool (num_samples) - истинные метки
+        '''
+        precision = 0
+        recall = 0
+        accuracy = 0
+        f1 = 0
 
-    # TODO: implement metrics!
-    # Some helpful links:
-    # https://en.wikipedia.org/wiki/Precision_and_recall
-    # https://en.wikipedia.org/wiki/F1_score
-    
-    return precision, recall, f1, accuracy
+        precision = TP / (TP + FP)
+        recall = TP / (TP + FN)
+        accuracy = (TP + TN) / (TP + TN + FP + FN)
+        f1 = 2 * (precision * recall) / (precision + recall)
+
+        return precision, recall, f1, accuracy
+
+    else:
+        raise ValueError('')
 
 
 def multiclass_accuracy(prediction, ground_truth):
-    '''
-    Computes metrics for multiclass classification
 
-    Arguments:
-    prediction, np array of int (num_samples) - model predictions
-    ground_truth, np array of int (num_samples) - true labels
-
-    Returns:
-    accuracy - ratio of accurate predictions to total samples
     '''
-    # TODO: Implement computing accuracy
-    return 0
+    Аргументы:
+    prediction, np-массив int (num_samples) - модельные предсказания
+    ground_truth, np-массив int (num_samples) - истинные метки
+    '''
+
+    if len(prediction) == len(ground_truth):
+        general = len(prediction)
+
+        count_true = 0
+        for pred in range(prediction):
+            for truth in range(ground_truth):
+                if pred == truth: count_true += 1
+
+        accuracy = count_true / general
+
+        return accuracy
+
+    else:
+        raise ValueError('')
